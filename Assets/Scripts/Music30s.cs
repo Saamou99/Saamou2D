cc310dee059406569937364d8df06809a07e3789
Saamou <90028820+Saamou99@users.noreply.github.com>
2023-02-23 02:43:34 +0100

    Fourth Commit
    4th Commit AudioHAX

--- Assets/Scripts/Music30s.cs
+++ Assets/Scripts/Music30s.cs
@@ -6,12 +6,30 @@ public class Music30s : MonoBehaviour
 {
     public AudioClip audioClip;
     private AudioSource audioSource;
+    private float fadeTime = 10f;
 
     void Start()
     {
         audioSource = GetComponent<AudioSource>();
         audioSource.clip = audioClip;
         audioSource.time = 30f; // Set the playback position to 30 seconds
+        audioSource.volume = 0f;
         audioSource.Play();
+
+        StartCoroutine(FadeIn());    
     }
+
+IEnumerator FadeIn()
+{
+    float currentTime = 0f;
+    
+    while (currentTime < fadeTime)
+    {
+        currentTime += Time.deltaTime;
+        audioSource.volume = Mathf.Lerp(0f, 0.9f, currentTime / fadeTime);
+        yield return null;
+    }
+    audioSource.volume = 0.9f;
+}
+
 }
