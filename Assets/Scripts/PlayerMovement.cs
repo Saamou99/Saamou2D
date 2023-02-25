25302f9a0d6194037a3d59af7da2a8ebc3805e79
Saamou <90028820+Saamou99@users.noreply.github.com>
2023-02-25 20:07:24 +0100

    Sitxh Commit
    Scene4 almost done

--- Assets/Scripts/PlayerMovement.cs
+++ Assets/Scripts/PlayerMovement.cs
@@ -7,10 +7,13 @@ public class PlayerMovement : MonoBehaviour
 {
     [SerializeField] private float speed;
     private Rigidbody2D rbody;
+    private Animator animator;
+    private bool grounded;
 
     private void Awake()
-    {
+    {   // Grab reference from Rigidbody2d and Animator from object;
         rbody = GetComponent<Rigidbody2D>();
+        animator = GetComponent<Animator>();
     }
 
     private void Update()
@@ -27,10 +30,29 @@ public class PlayerMovement : MonoBehaviour
         {
             transform.localScale = new Vector3(-1, 1, 1);
         }
-        
-        if (Input.GetKey(KeyCode.Space))
+
+        if (Input.GetKey(KeyCode.Space) && grounded)
         {
-            rbody.velocity = new Vector2(rbody.velocity.x, speed);
+          Jump();
+        }
+        
+        //Set Animator parameters;
+        animator.SetBool("Run", horizontalInput != 0);
+        animator.SetBool("Grounded", grounded);
+    }
+
+    private void Jump()
+    {
+        rbody.velocity = new Vector2(rbody.velocity.x, speed);
+        animator.SetTrigger("Jump");        
+        grounded = false;
+    }
+
+    private void OnCollisionEnter2D(Collision2D collision)
+    {
+        if (collision.gameObject.tag == "Ground")
+        {
+            grounded = true;
         }
     }
 }
